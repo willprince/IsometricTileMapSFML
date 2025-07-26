085b67baf96e5b1275ae46d6409e6314fa2f7ac0
will <williamprince9000@gmail.com>
2025-07-26 00:08:24 -0400

    add fps

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <sstream>
+#include <chrono>
 
 int TILE_WIDTH = 64;
 int TILE_HEIGHT = 32;
@@ -27,15 +28,12 @@ int main()
     sf::ConvexShape tileShape = generateTile(TILE_WIDTH, TILE_HEIGHT, sf::Color::Green);
     tileShape.setPosition(CENTER_TILE_COORDINATE);
 
-    sf::Vector2f tileApex = gridToScreen(x,y,TILE_WIDTH,TILE_HEIGHT);
-
     sf::Font font;
     font.loadFromFile("JetBrainsMono-Regular.ttf");
     sf::Text coordinateText;
     coordinateText.setFont(font);
-
-    coordinateText.setColor(sf::Color::Green);
-    coordinateText.setPosition(500.0f,500.0f);
+    sf::Text fpsText;
+    fpsText.setFont(font);
     
     bool inputEnable = false;
 
@@ -46,8 +44,20 @@ int main()
     view.reset(sf::FloatRect(SCREEN_WIDTH/2 - 600, SCREEN_HEIGHT/2 - 300, 1200, 600));
     window.setView(view);
 
+    coordinateText.setFillColor(sf::Color::Green);
+    coordinateText.setPosition(SCREEN_WIDTH/2 - 600, SCREEN_HEIGHT/2 - 300);
+    fpsText.setFillColor(sf::Color::Green);
+    fpsText.setPosition(SCREEN_WIDTH/2 - 600, SCREEN_HEIGHT/2 - 300 + coordinateText.getCharacterSize() + 10);
+    
+    window.setFramerateLimit(120);
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::high_resolution_clock::time_point end;
+    float fps;
+
     while (window.isOpen())
     {
+        start = std::chrono::high_resolution_clock::now();
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -95,7 +105,17 @@ int main()
         // DRAWING ZONE
         drawTileMap(window, grid, TILE_WIDTH, TILE_HEIGHT, whiteTileShape);
         window.draw(coordinateText);
+        window.draw(fpsText);
         window.draw(tileShape);
+
+        end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> deltaTime = end - start;
+        fps = (float)1e9/(float)std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+        std::stringstream fpsStream;
+        fpsStream << "FPS: " << (int)fps;
+        fpsText.setString(fpsStream.str());
+
+
         window.display();
     }
 
